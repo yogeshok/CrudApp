b16d83d20f2191902df9e101243651c54a122561
Yogesh Kumar <yogiky2002@gmail.com>
2022-02-18 23:56:20 +0530

    data displayed from database json file

--- src/Components/AllTasks.jsx
+++ src/Components/AllTasks.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { TableBody, TableCell, TableHead, TableRow, Table } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { getUsers } from "../Service/api";
 
 
 const AllTasks = () => {
 
+    const [users, setUsers] = useState([]);
     useEffect(() => {
             getAllUsers();
     }, []);
@@ -11,9 +13,35 @@ const AllTasks = () => {
     const getAllUsers = async () => {
         const response = await getUsers();
         console.log(response.data);
+        setUsers(response.data);
     }
     return (
-        <p>HI from user</p>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    users.map(user => {
+                        return (
+                        <TableRow>
+                            <TableCell>{user.id}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.username}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.phone}</TableCell>
+                        </TableRow>
+                        )
+                    })
+                }
+            </TableBody>
+        </Table>
     )
 }
 
