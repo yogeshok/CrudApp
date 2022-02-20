f33222c99a6b276e715d8ac83a5fb7ca1c53f12c
Yogesh Kumar <yogiky2002@gmail.com>
2022-02-20 23:21:56 +0530

    added component form

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -1,10 +1,39 @@
+import { FormControl, FormGroup, InputLabel, Input, Button, makeStyles, Typography } from "@material-ui/core";
 
+const useStyle = makeStyles({
+    container: {
+        width: "50%",
+        margin: "5% 0 0 25%",
+        '& > *': {
+            marginTop: 20
+        }
+    }
+})
 
 const AddTask = () => {
+    const classes = useStyle();
     return (
-        <p>
-            Add your Task here
-        </p>
+        
+        <FormGroup className={classes.container}>
+            <Typography variant="h4">Add Task</Typography>
+            <FormControl>
+                <InputLabel>Name</InputLabel>
+                <Input />
+            </FormControl>
+            <FormControl>
+                <InputLabel>Username</InputLabel>
+                <Input />
+            </FormControl>
+            <FormControl>
+                <InputLabel>Email</InputLabel>
+                <Input />
+            </FormControl>
+            <FormControl>
+                <InputLabel>Phone</InputLabel>
+                <Input />
+            </FormControl>
+            <Button variant="contained" color="primary">Add Task</Button>
+        </FormGroup>
     )
 }
 
